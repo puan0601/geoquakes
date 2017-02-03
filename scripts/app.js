228869003c57adaa762290a3cede443db8268333
puan0601 <anton.pugach@gmail.com>
2017-02-02 21:38:54 -0800

    fixed results display and got google maps working

--- scripts/app.js
+++ scripts/app.js
@@ -4,5 +4,19 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 $(document).on("ready", function() {
 
   // CODE IN HERE!
+  $.ajax({
+    url: weekly_quakes_endpoint,
+    success: onSuccess
+  });
 
-});
+  function onSuccess(quakes) {
+    console.log(quakes);
+    $.map(quakes.features, function (quake,idx) {
+      console.log(quake, idx);
+      $('#info').append(`<p>${quake.properties.title}</p>`);
+      // initMap();
+
+    });
+  }
+}
+);
